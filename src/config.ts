47eb0efe73f1361cf7567228cf8fbdd8746822ac
LennyFace24 <cathydy767@gmail.com>
2026-05-19 19:10:39 +0800

    feat:更新背景图片可占据整个屏幕而不仅仅是横幅

--- src/config.ts
+++ src/config.ts
@@ -19,7 +19,7 @@ export const siteConfig: SiteConfig = {
 	// 网站上面的横幅
 	banner: {
 		enable: true,
-		src: "assets/images/demo-banner.png", // Relative to the /src directory. Relative to the /public directory if it starts with '/'
+		src: "assets/images/noel.png", // Relative to the /src directory. Relative to the /public directory if it starts with '/'
 		position: "center", // Equivalent to object-position, only supports 'top', 'center', 'bottom'. 'center' by default
 		credit: {
 			enable: false, // Display the credit text of the banner image
